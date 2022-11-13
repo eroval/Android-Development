43a9e6e3bf2e71c47c931a6c61093c124f541695
Denis Zangarov <denisimo_98@yahoo.com>
2022-11-13 18:49:56 +0200

    update

--- AdnroidTestEx/app/src/main/java/com/example/adnroidtestex/MainActivity.java
+++ AdnroidTestEx/app/src/main/java/com/example/adnroidtestex/MainActivity.java
@@ -2,10 +2,18 @@ package com.example.adnroidtestex;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.app.RemoteInput;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,20 +23,28 @@ public class MainActivity extends AppCompatActivity {
     private Button btnStop;
     private Button btnGo;
 
+    private String CHANNEL_ID = "my_custom_channel";
+    private int notificationId = 1465;
+
+    private String KEY_TEXT_REPLY = "user_reply_key";
+    private String replyLabel = "Send Text";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnStop = (Button) findViewById(R.id.start);
-        btnStart = (Button) findViewById(R.id.stop);
+        btnStop = (Button) findViewById(R.id.stop);
+        btnStart = (Button) findViewById(R.id.start);
         btnGo = (Button) findViewById(R.id.go);
+
+        createNotificationChannel();
 
         btnGo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                goToActivity2();
+                goToSecondaryActivity();
             }
         });
 
@@ -38,31 +54,83 @@ public class MainActivity extends AppCompatActivity {
                 sendNotification();
             }
         });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { stopNotification(); }
+        });
+
     }
 
-    private void goToActivity2(){
-        Intent intent = new Intent(this, SecondaryActivity.class);
-        startActivity(intent);
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "channel_name";
+            String description = "channel_description";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
-
 
     private void sendNotification(){
-        Intent intent = new Intent(this, SecondaryActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,0);
+        RemoteInput remoteInput = new RemoteInput.Builder(KEY_TEXT_REPLY)
+                .setLabel(replyLabel)
+                .build();
+
+
+        Intent secondaryIntent = new Intent(this, SecondaryActivity.class);
+//        secondaryIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        secondaryIntent.setAction(Intent.ACTION_MAIN);
+        stackBuilder.addNextIntentWithParentStack(secondaryIntent);
+        PendingIntent pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Action action =
+                new NotificationCompat.Action.Builder(android.R.drawable.ic_input_add, replyLabel, pendingIntent)
+                .addRemoteInput(remoteInput)
+                .setAllowGeneratedReplies(true)
+                .build();
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-        .setSmallIcon(android.R.drawable.ic_dialog_alert)
-        .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
-        .setContentTitle("Notifications Title")
-        .setContentText("Your notification content here.")
-        .setSubText("Tap to view the website.")
-        .setStyle(new NotificationCompat.BigTextStyle()
-                .bigText("Your notification content here. ...Much longer text that cannot fit one line..."))
-        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-        // Set the intent that will fire when the user taps the notification
-        .setContentIntent(pendingIntent)
-        .setAutoCancel(true);
+                .addAction(action)
+                .setSmallIcon(android.R.drawable.ic_dialog_alert)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                .setContentTitle("MyAppTest")
+                .setContentText("")
+                .setSubText("Enter your text in the reply")
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("Continue please..."))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                // Set the intent that will fire when the user taps the notification
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(notificationId, builder.build());
 
     }
 
+//}
+
+    private void stopNotification() {
+        NotificationManager notificationManager = (NotificationManager) getApplicationContext()
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.cancel(notificationId);
+    }
+
+    private void goToSecondaryActivity(){
+        Intent secondaryIntent = new Intent(this, SecondaryActivity.class);
+        secondaryIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(secondaryIntent);
+    }
 
 }
--- AdnroidTestEx/app/src/main/java/com/example/adnroidtestex/SecondaryActivity.java
+++ AdnroidTestEx/app/src/main/java/com/example/adnroidtestex/SecondaryActivity.java
@@ -1,14 +1,50 @@
 package com.example.adnroidtestex;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.app.RemoteInput;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondaryActivity extends AppCompatActivity {
+    private String CHANNEL_ID = "my_custom_channel";
+    private int notificationId = 1465;
+
+    private String KEY_TEXT_REPLY = "user_reply_key";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secondary);
+
+        TextView textView = (TextView) findViewById(R.id.userInput);
+        textView.setText(getMessageText(getIntent()));
+
+        receiveNotification();
+    }
+
+    private CharSequence getMessageText(Intent intent) {
+        Bundle remoteInput = RemoteInput.getResultsFromIntent(intent);
+        if (remoteInput != null) {
+            return remoteInput.getCharSequence(KEY_TEXT_REPLY);
+        }
+        return null;
+    }
+
+    private void receiveNotification(){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentText("saved");
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        //update notification
+        notificationManager.notify(notificationId, builder.build());
     }
 }
